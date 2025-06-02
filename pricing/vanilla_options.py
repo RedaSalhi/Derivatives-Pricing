6146ded3e81810fd77bbabb4392ea65507c49121
RedaSalhi <salhi.reda47@gmail.com>
2025-06-02 14:52:03 +0100

    Update vanilla_options.py

--- pricing/vanilla_options.py
+++ pricing/vanilla_options.py
@@ -2,41 +2,31 @@
 
 from .models.black_scholes import black_scholes_price
 from .models.binomial_tree import binomial_tree_price
+from .models.longstaff_schwartz import longstaff_schwartz_price
+from .models.monte_carlo import monte_carlo_price
 
-def price_vanilla_option(
-    option_type,
-    exercise_style,
-    model,
-    **kwargs
-):
-    """
-    Master pricing function for vanilla options.
-
-    Parameters:
-        option_type (str): 'call' or 'put'
-        exercise_style (str): 'european' or 'american'
-        model (str): 'black-scholes' or 'binomial'
-        kwargs: All pricing params (S, K, T, r, sigma, q, N...)
-
-    Returns:
-        float: option price
-    """
-
+def price_vanilla_option(option_type, exercise_style, model, **kwargs):
     option_type = option_type.lower()
     exercise_style = exercise_style.lower()
     model = model.lower()
 
     if exercise_style not in ["european", "american"]:
-        raise ValueError("exercise_style must be 'european' or 'american'")
+        return "exercise_style must be 'european' or 'american'"
 
     if model == "black-scholes":
         if exercise_style == "american":
-            raise ValueError("Black-Scholes model does not support American options.")
+            return "Black-Scholes does not support American options."
         return black_scholes_price(option_type=option_type, **kwargs)
 
     elif model == "binomial":
         american_flag = exercise_style == "american"
         return binomial_tree_price(option_type=option_type, american=american_flag, **kwargs)
+
+    elif model == "monte-carlo":
+        if exercise_style == "american":
+            return longstaff_schwartz_price(option_type=option_type, **kwargs)
+        else:
+            return monte_carlo_price(option_type=option_type, **kwargs)
 
     else:
         raise ValueError(f"Unknown model: {model}")
