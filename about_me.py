09e43d0a52dc1c6dea75fc10ab9292fb9a7c4540
RedaSalhi <salhi.reda47@gmail.com>
2025-06-04 13:54:20 +0100

    Update about_me.py

--- about_me.py
+++ about_me.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 
 st.header("About Me")
-    st.markdown(
-        "This section can include your CV, a link to your "
-        "[LinkedIn](https://www.linkedin.com/) profile, and showcase other projects."
-    )
+st.markdown(
+    "This section can include your CV, a link to your "
+    "[LinkedIn](https://www.linkedin.com/) profile, and showcase other projects."
+)
