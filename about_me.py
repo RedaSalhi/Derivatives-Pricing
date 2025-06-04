7020af952e4b1bc40223288d2357c8962ffd57c1
RedaSalhi <salhi.reda47@gmail.com>
2025-06-04 13:51:06 +0100

    Create about_me.py

---
+++ about_me.py
@@ -0,0 +1,5 @@
+st.header("About Me")
+    st.markdown(
+        "This section can include your CV, a link to your "
+        "[LinkedIn](https://www.linkedin.com/) profile, and showcase other projects."
+    )
