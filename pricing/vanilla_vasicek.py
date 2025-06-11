d930d603759ff7795e332859ab2a8c6b0a1b5dcc
RedaSalhi <salhi.reda47@gmail.com>
2025-06-11 23:57:38 +0100

    Update vanilla_vasicek.py

--- pricing/vanilla_vasicek.py
+++ pricing/vanilla_vasicek.py
@@ -1,23 +1,17 @@
 #pricing/vanilla_vasicek.py
 
+# from pricing.vanilla_vasicek import price_zero_coupon, price_coupon_bond, price_bond_option
+
+from pricing.models.interest_rates.analytical_vasicek import vasicek_zero_coupon_price, price_coupon_bond
+
+
 def price_zero_coupon(r_t, t, T, a, lam, sigma, face_value=1.0, model="Analytical"):
-    if model == "Analytical":
-        from pricing.models.interest_rates.analytical_vasicek import vasicek_zero_coupon_price
         return vasicek_zero_coupon_price(r_t, t, T, a, lam, sigma, face_value)
-    elif model == "Monte Carlo":
-        raise NotImplementedError("Zero-coupon pricing via Monte Carlo is not supported (use analytical)")
-    else:
-        raise ValueError("Model must be 'Analytical' or 'Monte Carlo'.")
 
 
 def price_coupon_bond(rates, a, lam, sigma, maturity=5, coupon=0.05, face_value=1.0, dt=0.5, model="Analytical"):
     if model == "Analytical":
-        from pricing.models.interest_rates.analytical_vasicek import price_coupon_bond
         return price_coupon_bond(rates, a, lam, sigma, maturity, coupon, face_value, dt)
-    elif model == "Monte Carlo":
-        raise NotImplementedError("Coupon bond pricing via Monte Carlo is not implemented yet.")
-    else:
-        raise ValueError("Model must be 'Analytical' or 'Monte Carlo'.")
 
 
 def price_bond_option(r0, a, lam, sigma, T1, T2, K, dt=1/12, face=1.0, option_type='call', model="Analytical", n_paths=10000):
@@ -27,6 +21,6 @@ def price_bond_option(r0, a, lam, sigma, T1, T2, K, dt=1/12, face=1.0, option_ty
     elif model == "Monte Carlo":
         from pricing.models.interest_rates.monte_carlo_vasicek import vasicek_bond_option_price_mc
         price, std = vasicek_bond_option_price_mc(r0, a, lam, sigma, T1, T2, K, dt, n_paths, face, option_type)
-        return price
+        return price 
     else:
         raise ValueError("Model must be 'Analytical' or 'Monte Carlo'.")
