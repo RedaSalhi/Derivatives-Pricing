a9c7c0002b802ffb4fae638343e5385bd8eec510
RedaSalhi <salhi.reda47@gmail.com>
2025-06-12 00:45:45 +0100

    Update vanilla_vasicek.py

--- pricing/vanilla_vasicek.py
+++ pricing/vanilla_vasicek.py
@@ -9,8 +9,8 @@ def price_zero_coupon(r_t, t, T, a, lam, sigma, face_value=1.0):
         return vasicek_zero_coupon_price(r_t, t, T, a, lam, sigma, face_value)
 
 
-def price_coupon_bond(rates, a, lam, sigma, maturity=5, coupon=0.05, face_value=1.0, dt=0.5):
-        return price_coupon_bond(rates, a, lam, sigma, maturity, coupon, face_value, dt)
+def price_coupon_bond(rates, a, lam, sigma, maturity=5, coupon=0.05, face=1.0, dt=0.5):
+        return price_coupon_bond(rates, a, lam, sigma, maturity, coupon, face, dt)
 
 
 def price_bond_option(r0, a, lam, sigma, T1, T2, K, dt, face=1.0, option_type='call', model="Analytical", n_paths=10000):
