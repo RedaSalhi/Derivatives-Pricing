cd217899f2b95b6a24a0dfc47b6ad8a2f9d16599
RedaSalhi <salhi.reda47@gmail.com>
2025-06-11 23:59:52 +0100

    Update vanilla_vasicek.py

--- pricing/vanilla_vasicek.py
+++ pricing/vanilla_vasicek.py
@@ -5,19 +5,18 @@
 from pricing.models.interest_rates.analytical_vasicek import vasicek_zero_coupon_price, price_coupon_bond
 
 
-def price_zero_coupon(r_t, t, T, a, lam, sigma, face_value=1.0, model="Analytical"):
+def price_zero_coupon(r_t, t, T, a, lam, sigma, face_value=1.0):
         return vasicek_zero_coupon_price(r_t, t, T, a, lam, sigma, face_value)
 
 
-def price_coupon_bond(rates, a, lam, sigma, maturity=5, coupon=0.05, face_value=1.0, dt=0.5, model="Analytical"):
-    if model == "Analytical":
+def price_coupon_bond(rates, a, lam, sigma, maturity=5, coupon=0.05, face_value=1.0, dt=0.5):
         return price_coupon_bond(rates, a, lam, sigma, maturity, coupon, face_value, dt)
 
 
-def price_bond_option(r0, a, lam, sigma, T1, T2, K, dt=1/12, face=1.0, option_type='call', model="Analytical", n_paths=10000):
+def price_bond_option(r0, a, lam, sigma, T1, T2, K, dt, face=1.0, option_type='call', model="Analytical", n_paths=10000):
     if model == "Analytical":
         from pricing.models.interest_rates.analytical_vasicek import vasicek_bond_option_price
-        return vasicek_bond_option_price(r0, 0, T1, T2, K, a, lam, sigma, face, option_type)
+        return vasicek_bond_option_price(r0, T1, T2, K, a, lam, sigma, face, option_type)
     elif model == "Monte Carlo":
         from pricing.models.interest_rates.monte_carlo_vasicek import vasicek_bond_option_price_mc
         price, std = vasicek_bond_option_price_mc(r0, a, lam, sigma, T1, T2, K, dt, n_paths, face, option_type)
